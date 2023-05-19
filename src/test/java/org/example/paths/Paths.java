5056f820941cb27d5cb0457ff967e5fb3df00c30
Nikita Kazak <kitkazak@yandex.ru>
2023-05-19 22:02:37 +0300

    Added path constants

---
+++ src/test/java/org/example/paths/Paths.java
@@ -0,0 +1,7 @@
+package org.example.paths;
+
+public final class Paths {
+    private static final String baseURI = "https://qa-scooter.praktikum-services.ru/api/v1";
+    public static final String courier = baseURI + "/courier";
+    public static final String courierLogin = baseURI + "/courier/login";
+}
